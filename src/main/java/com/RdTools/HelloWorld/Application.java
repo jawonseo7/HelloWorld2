6dddcb55d2afb7e697d447712494b58d223cac46
405hoseoci <405hoseoci@hanmail.net>
2021-05-24 08:15:27 +0900

    Controller 추가

--- src/main/java/com/RdTools/HelloWorld/Application.java
+++ src/main/java/com/RdTools/HelloWorld/Application.java
@@ -1,11 +1,13 @@
-package com.rdtools.springboot;
+package com.RdTools.HelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/main/java/com/RdTools/HelloWorld/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.RdTools.HelloWorld.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
