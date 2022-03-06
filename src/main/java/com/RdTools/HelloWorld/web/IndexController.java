8a150f09a2cdb3a5c6368edaaf90fcc2ec52fe46
jawon.seo <jawonseo7@gmail.com>
2022-03-06 12:44:34 +0900

    OAuth2 적용

--- src/main/java/com/RdTools/HelloWorld/web/IndexController.java
+++ src/main/java/com/RdTools/HelloWorld/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.RdTools.HelloWorld.web;
 
+import com.RdTools.HelloWorld.config.auth.LoginUser;
+import com.RdTools.HelloWorld.config.auth.dto.SessionUser;
 import com.RdTools.HelloWorld.service.PostsService;
 import com.RdTools.HelloWorld.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +11,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
