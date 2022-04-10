ba3064afb58d6b05ad7db80908867a70b7da494e
405hoseoci <405hoseoci@hanmail.net>
2022-04-10 11:45:36 +0900

    log 추가

--- src/main/java/com/RdTools/HelloWorld/Application.java
+++ src/main/java/com/RdTools/HelloWorld/Application.java
@@ -3,11 +3,27 @@ package com.RdTools.HelloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
 
 //@EnableJpaAuditing
+@Log4j2
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
+
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping(value="/log")
+    public void log() throws Exception {
+
+        //FATAL, ERROR, WARN, INFO, DEBUG, TRACE
+        log.fatal("FATAL");
+        log.error("ERROR");
+        log.warn("WARN");
+        log.info("INFO");
+        log.debug("DEBUG");
+        log.trace("TRACE");
     }
 }
